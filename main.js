5839030f1a6534f9f983f3cf05547f5bac148be6
omgjwood <justinemwood@gmail.com>
2017-01-10 15:13:08 -0600

    trying to change the h1

--- main.js
+++ main.js
@@ -3,3 +3,8 @@
  */
 var myVar = "Hello World";
 console.log (myVar);
+
+// changing h1 tag using vanllia js
+var element = document.getElementById("header");
+console.log(element);
+element.innerHTML = "Hello World";
