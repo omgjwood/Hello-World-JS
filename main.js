6ec2db8bdc9d4c17b7e3d187c31aebc13610fb05
Joel Norris <joel.r.norris@gmail.com>
2017-01-17 20:09:13 -0600

    added init function and kicker script to bottom of body

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 /**
  * Created by justinemariewood on 1/10/17.
  */
-var myVar = "Hello World";
-console.log (myVar);
 
-// changing h1 tag using vanllia js
-var element = document.getElementById("header");
-console.log(element);
-element.innerHTML = "Hello World";
+function init() {
+
+    var myVar = "Hello World";
+    var element;
+    console.log(myVar);
+
+    // changing h1 tag using vanllia js
+    element = document.getElementById("header");
+
+    console.log(element);
+
+    element.innerHTML = "Hello World";
+
+}
+
+
