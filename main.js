5e22b70fb8b036f68b5f69ea6668b3960163fbf3
omgjwood <justinemwood@gmail.com>
2017-01-21 08:56:55 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by justinemariewood on 1/21/17.
+ */
+var myVar = "Hello World";
+console.log (myVar);
+
+// changing h1 tag using vanllia js
+var element = document.getElementsByClassName("trying");
+console.log(element);
+element.innerHTML = "I did It!!";
