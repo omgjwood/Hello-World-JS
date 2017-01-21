28f647d8f36b0851209d9714963b1c2fcea32d44
omgjwood <justinemwood@gmail.com>
2017-01-21 09:33:30 -0600

    showing in the console but not showing in html

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var myVar = "Hello World";
 console.log (myVar);
 
 // changing h1 tag using vanllia js
-var element = document.getElementsByClassName("trying");
-console.log(element);
-element.innerHTML = "I did It!!";
+
+var element;
+element = document.getElementsByClassName('trying').textContent = "I'm getting it!!";
+console.log (element);localStorage
